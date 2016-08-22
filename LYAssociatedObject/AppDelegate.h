828550dce12eeff1ceb1ef289bd1546bfa95686a
Gordon <liyang_xx@icloud.com>
2016-08-22 14:55:22 +0800

    add files

---
+++ LYAssociatedObject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYAssociatedObject
+//
+//  Created by Gordon on 16/8/22.
+//  Copyright © 2016年 Gordon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LYAssociatedObjectTests/LYAssociatedObjectTests.m
@@ -0,0 +1,39 @@
+//
+//  LYAssociatedObjectTests.m
+//  LYAssociatedObjectTests
+//
+//  Created by Gordon on 16/8/22.
+//  Copyright © 2016年 Gordon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LYAssociatedObjectTests : XCTestCase
+
+@end
+
+@implementation LYAssociatedObjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
